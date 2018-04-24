a5c5ad563103bbea0e3b319b2f7b9dbe1ffba8a9
Larry Miles <lmiles@lundinoir.com>
2018-04-24 09:01:00 +0100

    commit to add alex jones updates

--- app/routes.js
+++ app/routes.js
@@ -17,6 +17,7 @@ router.post('/', function (req, res) {
     var prosecutor = req.session.data['prosecutor-group']
     
     req.session.data['addDatesToAvoidCount'] = "3"
+    req.session.data['whichPage'] = "Not yet specified"
 
     if (prosecutor == "TfL") {
         
@@ -55,6 +56,42 @@ router.post('/', function (req, res) {
         req.session.data['statement-of-facts'] = "I, Inspector Morse, was suspicious as the defendant kept moving seats away from me. When asked for a ticket he replied “I don’t have one as I am not really on this train, you’re seeing an illusion”. I had no choice but to issue him with a ticket."
         
         req.session.data['new-case-offence-wording'] = "You have been charged with the following offence: That you, on 24 March 2018 at HIGH STREET, W3 Being a passenger on a Public Service Vehicle operated on behalf of London Bus Services Limited being used for the carriage of passengers at separate fares, did use in relation to the journey you were taking a ticket which had been issued for use by another person on terms that it is not transferable. Contrary to Regulation 7(1)(b) of the Public Service Vehicles (conduct of Drivers, Inspectors, Conductors & Passengers) Regulations 1990 SI No. 1020 and contrary to Section 25(3) of the Public Passenger Vehicles Act 1981."
+        
+        // *************
+        //CASE DETAILS 2
+        req.session.data['case-reference-2'] = "9060951"
+        
+        // Case details tab
+        req.session.data['case-status-2'] = "Not guilty"
+        
+        req.session.data['prosecutor-2'] = "TfL"
+        req.session.data['notice-served-2'] = "28 February 2018"
+        req.session.data['notice-served-short-2'] = "28 Feb 2018"
+        req.session.data['plea-entry-2'] = "26 April 2018"
+        req.session.data['plea-entry-short-2'] = "26 Apr 2018"
+
+        // Personal details tab
+        req.session.data['defendent-first-name-2'] = "Alex"
+        req.session.data['defendent-last-name-2'] = "JONES"
+        req.session.data['defendent-dob-2'] = "17 August 1974"
+        req.session.data['defendent-dob-short-2'] = "17 Aug 1974"
+        req.session.data['defendent-age-2'] = "44"
+        req.session.data['defendent-address-line-1-2'] = "46 Milton Grove"
+        req.session.data['defendent-address-line-2-2'] = "Stoke Newington"
+        req.session.data['defendent-address-line-3-2'] = ""
+        req.session.data['defendent-address-town-or-city-2'] = "London"
+        req.session.data['defendent-address-postcode-2'] = "N16 8QY"
+        req.session.data['defendent-email-2'] = "jones-alex@gmail.com"
+        req.session.data['defendent-home-telephone-2'] = ""
+        req.session.data['defendent-mobile-2'] = "07766280294"
+        req.session.data['defendent-nin-2'] = "AJ 54 36 22 B"
+
+        //Offence details
+        req.session.data['offence-title-2'] = "Passenger used ticket issued for another person"
+        req.session.data['offence-description-2'] = "On 10/06/2014 At grosvenor place SW1 Being a passenger on a Public Service Vehicle operated on behalf of London Bus Services Limited being used for the carriage of passengers at separate fares where the vehicle was being operated by a Driver without a Conductor did not immediately on boarding pay the fare to the Driver for the journey you intended to take."
+        req.session.data['statement-of-facts-2'] = "I, Inspector Morse, was suspicious as the defendant kept moving seats away from me. When asked for a ticket he replied “I don’t have one as I am not really on this train, you’re seeing an illusion”. I had no choice but to issue him with a ticket."
+        
+        req.session.data['new-case-offence-wording-2'] = "You have been charged with the following offence: That you, on 24 March 2018 at HIGH STREET, W3 Being a passenger on a Public Service Vehicle operated on behalf of London Bus Services Limited being used for the carriage of passengers at separate fares, did use in relation to the journey you were taking a ticket which had been issued for use by another person on terms that it is not transferable. Contrary to Regulation 7(1)(b) of the Public Service Vehicles (conduct of Drivers, Inspectors, Conductors & Passengers) Regulations 1990 SI No. 1020 and contrary to Section 25(3) of the Public Passenger Vehicles Act 1981."
         
         //res.redirect('/prosecutor/case-details-page')
         res.redirect('/prosecutor/dashboard')
@@ -119,14 +156,14 @@ router.post('/prosecutor/defendant-details-updated', function (req, res) {
 // ******************
 // Add dates to avoid
 router.post('/prosecutor/add-dates-to-avoid', function (req, res) {
-
+    
     res.redirect('/prosecutor/dates-to-avoid')
     
 })
 // **************
 // Dates to avoid
 router.post('/prosecutor/dates-to-avoid', function (req, res) {
-
+    
     res.redirect('/prosecutor/dates-to-avoid-confirmation')
     
 })
@@ -141,7 +178,7 @@ router.post('/prosecutor/dates-to-avoid-2', function (req, res) {
 // Dates to avoid - confirmation
 router.post('/prosecutor/dates-to-avoid-confirmation', function (req, res) {
 
-    req.session.data['addDatesToAvoidCount'] = "2"
+    req.session.data['addDatesToAvoidCount'] = req.session.data['addDatesToAvoidCount']-1
 
     res.redirect('/prosecutor/add-dates-to-avoid')
     
@@ -150,7 +187,7 @@ router.post('/prosecutor/dates-to-avoid-confirmation', function (req, res) {
 // Dates to avoid - confirmation 2
 router.post('/prosecutor/dates-to-avoid-confirmation-2', function (req, res) {
 
-    req.session.data['addDatesToAvoidCount'] = "2"
+    req.session.data['addDatesToAvoidCount'] = req.session.data['addDatesToAvoidCount']-1
 
     res.redirect('/prosecutor/case-details-page')
     
