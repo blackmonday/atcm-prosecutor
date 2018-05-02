2d5e6d4e829c4d57d71e423623d6678671894a14
Larry Miles <lmiles@lundinoir.com>
2018-05-02 08:45:23 +0100

    early morning updates

--- app/routes.js
+++ app/routes.js
@@ -16,7 +16,7 @@ router.post('/', function (req, res) {
 
     var prosecutor = req.session.data['prosecutor-group']
     
-    req.session.data['addDatesToAvoidCount'] = "3"
+    req.session.data['addDatesToAvoidCount'] = "2"
     req.session.data['whichPage'] = "Not yet specified"
 
     if (prosecutor == "TfL") {
@@ -133,6 +133,40 @@ router.post('/', function (req, res) {
 
         req.session.data['new-case-offence-wording'] = "Lorem ipsum dolor sit amet."
         
+        // *************
+        //CASE DETAILS 2
+        req.session.data['case-reference-2'] = "9060951"
+        
+        // Case details tab
+        req.session.data['case-status-2'] = "Not guilty"
+        
+        req.session.data['prosecutor-2'] = "TV Licensing"
+        req.session.data['notice-served-2'] = "28 February 2018"
+        req.session.data['notice-served-short-2'] = "28 Feb 2018"
+        req.session.data['plea-entry-2'] = "26 April 2018"
+        req.session.data['plea-entry-short-2'] = "26 Apr 2018"
+
+        // Personal details tab
+        req.session.data['defendent-first-name-2'] = "Alex"
+        req.session.data['defendent-last-name-2'] = "HAMMOND"
+        req.session.data['defendent-dob-2'] = "17 August 1974"
+        req.session.data['defendent-dob-short-2'] = "17 Aug 1974"
+        req.session.data['defendent-age-2'] = "44"
+        req.session.data['defendent-address-line-1-2'] = "46 Milton Grove"
+        req.session.data['defendent-address-line-2-2'] = "Stoke Newington"
+        req.session.data['defendent-address-line-3-2'] = ""
+        req.session.data['defendent-address-town-or-city-2'] = "London"
+        req.session.data['defendent-address-postcode-2'] = "N16 8QY"
+        req.session.data['defendent-email-2'] = "jones-alex@gmail.com"
+        req.session.data['defendent-home-telephone-2'] = ""
+        req.session.data['defendent-mobile-2'] = "07766280294"
+        req.session.data['defendent-nin-2'] = "AJ 54 36 22 B"
+
+        //Offence details
+        req.session.data['offence-title-2'] = "Possess/control TV set with intent another use install without a licence"
+        req.session.data['offence-description-2'] = "On 01/11/2017 at Chelmsford in the county of Essex were in possession or control of a colour television receiver knowing, or having reasonable grounds for believing, that another person intended to install or use the receiver without a licence."
+        req.session.data['statement-of-facts-2'] = "I, Inspector Morse, was suspicious as the defendant kept moving seats away from me. When asked for a ticket he replied “I don’t have one as I am not really on this train, you’re seeing an illusion”. I had no choice but to issue him with a ticket."
+        
         res.redirect('/prosecutor/dashboard')
                 
     } else if (prosecutor == "DVLA") {
@@ -172,6 +206,40 @@ router.post('/', function (req, res) {
 
         req.session.data['new-case-offence-wording'] = "Lorem ipsum dolor sit amet."
         
+        // *************
+        //CASE DETAILS 2
+        req.session.data['case-reference-2'] = "9060951"
+        
+        // Case details tab
+        req.session.data['case-status-2'] = "Not guilty"
+        
+        req.session.data['prosecutor-2'] = "DVLA"
+        req.session.data['notice-served-2'] = "28 February 2018"
+        req.session.data['notice-served-short-2'] = "28 Feb 2018"
+        req.session.data['plea-entry-2'] = "26 April 2018"
+        req.session.data['plea-entry-short-2'] = "26 Apr 2018"
+
+        // Personal details tab
+        req.session.data['defendent-first-name-2'] = "Alex"
+        req.session.data['defendent-last-name-2'] = "HAMMOND"
+        req.session.data['defendent-dob-2'] = "17 August 1974"
+        req.session.data['defendent-dob-short-2'] = "17 Aug 1974"
+        req.session.data['defendent-age-2'] = "44"
+        req.session.data['defendent-address-line-1-2'] = "46 Milton Grove"
+        req.session.data['defendent-address-line-2-2'] = "Stoke Newington"
+        req.session.data['defendent-address-line-3-2'] = ""
+        req.session.data['defendent-address-town-or-city-2'] = "London"
+        req.session.data['defendent-address-postcode-2'] = "N16 8QY"
+        req.session.data['defendent-email-2'] = "jones-alex@gmail.com"
+        req.session.data['defendent-home-telephone-2'] = ""
+        req.session.data['defendent-mobile-2'] = "07766280294"
+        req.session.data['defendent-nin-2'] = "AJ 54 36 22 B"
+
+        //Offence details
+        req.session.data['offence-title-2'] = "Keep a vehicle without a vehicle license"
+        req.session.data['offence-description-2'] = "On 01/11/2017 at Chelmsford in the county of Essex were in possession or control of a colour television receiver knowing, or having reasonable grounds for believing, that another person intended to install or use the receiver without a licence."
+        req.session.data['statement-of-facts-2'] = "I, Inspector Morse, was suspicious as the defendant kept moving seats away from me. When asked for a ticket he replied “I don’t have one as I am not really on this train, you’re seeing an illusion”. I had no choice but to issue him with a ticket."
+        
         res.redirect('/prosecutor/dashboard')
         
     }
@@ -185,6 +253,7 @@ router.post('/prosecutor/dashboard', function (req, res) {
     res.redirect('/prosecutor/search-for-a-case')
     
 })
+
 // *************************
 // Defendant details updated
 router.post('/prosecutor/defendant-details-updated', function (req, res) {
@@ -192,6 +261,7 @@ router.post('/prosecutor/defendant-details-updated', function (req, res) {
     res.redirect('/prosecutor/dashboard')
     
 })
+
 // ******************
 // Add dates to avoid
 router.post('/prosecutor/add-dates-to-avoid', function (req, res) {
@@ -199,6 +269,7 @@ router.post('/prosecutor/add-dates-to-avoid', function (req, res) {
     res.redirect('/prosecutor/dates-to-avoid')
     
 })
+
 // **************
 // Dates to avoid
 router.post('/prosecutor/dates-to-avoid', function (req, res) {
@@ -206,6 +277,7 @@ router.post('/prosecutor/dates-to-avoid', function (req, res) {
     res.redirect('/prosecutor/dates-to-avoid-confirmation')
     
 })
+
 // **************
 // Dates to avoid 2
 router.post('/prosecutor/dates-to-avoid-2', function (req, res) {
@@ -213,6 +285,7 @@ router.post('/prosecutor/dates-to-avoid-2', function (req, res) {
     res.redirect('/prosecutor/dates-to-avoid-confirmation-2')
     
 })
+
 // *****************************
 // Dates to avoid - confirmation
 router.post('/prosecutor/dates-to-avoid-confirmation', function (req, res) {
@@ -222,21 +295,17 @@ router.post('/prosecutor/dates-to-avoid-confirmation', function (req, res) {
     res.redirect('/prosecutor/add-dates-to-avoid')
     
 })
+
 // *****************************
 // Dates to avoid - confirmation 2
 router.post('/prosecutor/dates-to-avoid-confirmation-2', function (req, res) {
 
     req.session.data['addDatesToAvoidCount'] = req.session.data['addDatesToAvoidCount']-1
 
-    res.redirect('/prosecutor/case-details-page')
+    //res.redirect('/prosecutor/case-details-page')
+    res.redirect('/prosecutor/add-dates-to-avoid')
     
 })
-
-
-
-
-
-
 
 // *************************
 // Upload files for new case
@@ -245,6 +314,7 @@ router.post('/prosecutor/upload-files-for-new-case', function (req, res) {
     res.redirect('/prosecutor/add-details-for-new-case')
     
 })
+
 // ************************
 // Add details for new case
 router.post('/prosecutor/add-details-for-new-case', function (req, res) {
@@ -252,6 +322,7 @@ router.post('/prosecutor/add-details-for-new-case', function (req, res) {
     res.redirect('/prosecutor/check-new-case-details')
     
 })
+
 // **************************
 // Check new new case details
 router.post('/prosecutor/check-new-case-details', function (req, res) {
@@ -259,6 +330,7 @@ router.post('/prosecutor/check-new-case-details', function (req, res) {
     res.redirect('/prosecutor/create-a-new-case-confirmation')
     
 })
+
 // ******************************
 // Create a new case confirmation
 router.post('/prosecutor/create-a-new-case-confirmation', function (req, res) {
